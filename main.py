944e17e3dc6c74af518b5a7964c970058ca3c8cc
jeffgodwyll <jeffgodwyll@gmail.com>
2016-03-01 13:11:59 +0000

    Transition to ndb

--- main.py
+++ main.py
@@ -1,40 +1,86 @@
-from flask import Flask, jsonify, render_template
+from time import mktime
+from datetime import datetime
+import logging
+
+from google.appengine.ext import ndb
+from webapp2_extras.appengine.auth.models import Unique
+
+from flask import Flask, render_template
 import feedparser
+
 
 app = Flask(__name__)
 
 GOODREADS_RSS_URL = 'https://www.goodreads.com/quotes_of_the_day/rss'
 
 
-def parse_quotes(url):
-    feedparser._HTMLSanitizer.acceptable_elements = []
-    feed = feedparser.parse(url)
-    quotes = []
-    for feed in feed.entries:
-        quotes.append([a for a in feed.summary.splitlines() if a and a != '-'])
-    return quotes
+class Quote(ndb.Model):
+    text = ndb.StringProperty(required=False)
+    author = ndb.StringProperty(required=False)
+    # author_link = db.StringProperty(required=False)
+    link = ndb.StringProperty(required=False)
+    date = ndb.DateTimeProperty(required=False)
+    date_modified = ndb.DateTimeProperty(auto_now_add=True)
+
+    @classmethod
+    def create_quote(cls, link, date):
+        uniques = [
+            'Quote.link.%s' % link,
+            'Quote.date.%s' % date,
+        ]
+
+        # transactionally create the unique quote based on date and link
+        # https://webapp-improved.appspot.com/_modules/webapp2_extras/appengine/auth/models.html#Unique
+        success, existing = Unique.create_multi(uniques)
+        if success:
+            # the unique values were created so we can save the quote
+            quote = Quote(date=date, link=link)
+            quote.put()
+            return quote
 
 
-@app.route('/quotes/api/v1')
-def quotes():
-    quotes = parse_quotes(GOODREADS_RSS_URL)
-    return jsonify(quotes=quotes)
+def read_feed():
+    feedparser._HTMLSanitizer.acceptable_elements = []  # cleans up all html tag
+    feeds = feedparser.parse(GOODREADS_RSS_URL)
+
+    # q = db.Query('Quote')
+
+    for feed in feeds.entries:
+
+        clearer_date = datetime.fromtimestamp(mktime(feed.published_parsed))
+
+        uniques = ['Quote.link.%s' % feed.link,
+                   'Quote.date.%s' % clearer_date, ]
+        success, existing = Unique.create_multi(uniques)
+
+        if success:
+            quote = Quote()
+            quote.date = clearer_date
+            parsed_summary = [a for a in feed.summary.splitlines()
+                              if a and a != '-']
+            quote.text = parsed_summary[0]
+            quote.author = parsed_summary[1]
+            quote.link = feed.link
+            quote.put()
+            logging.info(
+                'New feed: {}, dated: {} has been inserted into the datastore'
+                .format(feed.link, clearer_date)
+            )
+        else:
+            logging.debug('Properties %r are not unique.' % existing)
 
 
-@app.route('/quotes/api/v1/<id>')
-def single_quote(id):
-    quotes = parse_quotes(GOODREADS_RSS_URL)
-    return jsonify({'id': id,
-                    'quote':
-                        {'summary': quotes[int(id)][0],
-                         'author': quotes[(int(id))][1]}
-                    })
+@app.route('/cron')
+def cron():
+    read_feed()
+    return 'updated quotes'
 
 
 @app.route('/')
 def home():
-    return render_template('quotes.html',
-                           quotes=parse_quotes(GOODREADS_RSS_URL))
+    quotes = Quote.query().order(-Quote.date).fetch(7)
+    return render_template('home.html', quotes=quotes)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
